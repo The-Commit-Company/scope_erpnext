6fced91e9d79b9951e4de9ea2d58d619f2f2dd5b
Nikhil Kothari <nikhil.kothari@thecommit.company>
2024-07-22 13:50:07 +0530

    feat: login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
-import './App.css'
-import { FrappeProvider, useFrappeGetDocList } from 'frappe-react-sdk'
+import { FrappeProvider } from 'frappe-react-sdk'
+import Login from './pages/Login'
 function App() {
-	const [count, setCount] = useState(0)
 
 	return (
 		<div className="App">
@@ -10,30 +8,10 @@ function App() {
 				siteName={import.meta.env.VITE_SITE_NAME}
 				socketPort={import.meta.env.VITE_SOCKET_PORT}
 			>
-				<div>
-					<TestComponent />
-					<h1>Vite + React + Frappe</h1>
-					<div className="card">
-						<button onClick={() => setCount((count) => count + 1)}>
-							count is {count}
-						</button>
-						<p>
-							Edit <code>src/App.jsx</code> and save to test HMR
-						</p>
-					</div>
-					<p className="read-the-docs">
-						Click on the Vite and React logos to learn more
-					</p>
-				</div>
+				<Login />
 			</FrappeProvider>
 		</div>
 	)
-}
-
-const TestComponent = () => {
-	const { data } = useFrappeGetDocList('User')
-
-	return <div></div>
 }
 
 export default App
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,78 @@
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useFrappeAuth } from "frappe-react-sdk"
+import { useState } from "react"
+
+const Login = () => {
+
+    const { currentUser, login, logout } = useFrappeAuth()
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    console.log(currentUser)
+
+    const handleSubmit = () => {
+        console.log(email, password)
+        login({
+            username: email,
+            password: password
+        })
+    }
+
+    return (
+        <div className="w-screen h-screen flex items-center justify-center">
+            {currentUser ? <div>Logged in as {currentUser}
+
+                <Button onClick={logout}>Logout</Button>
+            </div> :
+                <Card className="w-[350px]">
+                    <CardHeader>
+                        <CardTitle>Hello</CardTitle>
+                        <CardDescription>Login to your account</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <form>
+                            <div className="grid w-full items-center gap-4">
+                                <div className="flex flex-col space-y-1.5">
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input
+                                        id="email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        placeholder="jane.doe@example.com" />
+                                </div>
+                                <div className="flex flex-col space-y-1.5">
+                                    <Label htmlFor="password">Password</Label>
+                                    <Input
+                                        id="password"
+                                        type='password'
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                    />
+                                </div>
+                            </div>
+                        </form>
+                    </CardContent>
+                    <CardFooter className="flex justify-end">
+                        <Button
+                            onClick={handleSubmit}
+                        >Login</Button>
+                    </CardFooter>
+                </Card>
+            }
+        </div>
+
+    )
+}
+
+export default Login
